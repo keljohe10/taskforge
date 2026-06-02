b6114c87a9b5bae34c4b88a07eaf337412e18586
kelvin jose hernandez cabrera <kelhzkb@hotmail.com>
2026-06-02 18:57:53 +0200

    fix(ci/cd): Fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
 import neostandard from 'neostandard'
 
-export default neostandard({ ts: true })
+export default [
+  { ignores: ['**/dist/**', '**/build/**', '**/node_modules/**'] },
+  ...neostandard({ ts: true }),
+]
